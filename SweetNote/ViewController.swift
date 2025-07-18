e46060249d1df22ea1e79233cdd0755de038204f
KaMaDi <artkamadi@gmail.com>
2025-07-18 07:50:24 +0300

    Initial Commit

---
+++ SweetNote/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SweetNote
+//
+//  Created by Jedi on 18.07.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
